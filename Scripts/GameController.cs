223de5a76ffe2e978d8459bd9d401f96e308f2b1
Gumemura <gumemura123@gmail.com>
2021-05-08 18:11:28 -0300

    first commit

---
+++ Scripts/GameController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{ 
+	public Hero hero; //the controlable hero
+
+	private float halfWidthOfTheScreen; //x coordinate of half of the screen
+	private float halfHeightOfTheScreen; //x coordinate of half of the screen
+
+	private bool goingToJump; //used to prevent movement
+	private bool isJumping = false; //hero is jumping
+
+	void Start()
+	{
+		halfWidthOfTheScreen = Display.main.systemWidth / 2;
+		halfHeightOfTheScreen = Display.main.systemHeight / 2;
+	}
+
+	void Update()
+	{
+		foreach(Touch touch in Input.touches){
+			switch (touch.phase){
+				case TouchPhase.Began:
+					//jump
+					if(touch.position.y > halfHeightOfTheScreen && !isJumping){
+						hero.jump();
+						isJumping = true;
+					}else if(touch.position.y < halfHeightOfTheScreen){
+						hero.turnOnOffMovementAnim(true);
+					}
+					break;
+
+				case TouchPhase.Ended:
+					if(touch.position.y < halfHeightOfTheScreen){
+						hero.turnOnOffMovementAnim(false);
+					}
+					break;   
+			}
+
+			//movement
+			if(touch.position.y < halfHeightOfTheScreen){
+				if(touch.position.x > halfWidthOfTheScreen){
+					hero.move(1);//moving right
+				}else{
+					hero.move(-1);//moving left
+				}
+			}
+		}
+
+
+		isJumping = hero.triggerJumpAnim();
+		
+	}
+}
---
+++ Scripts/Hero.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hero : MonoBehaviour
+{ 
+	public float heroVelocity = 1;//movement velocity
+	public float jumpForce; //jump aceleration
+
+	private Animator heroAnimator;
+	private SpriteRenderer heroSpriteRenderer;
+	private Rigidbody2D heroRB;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        heroAnimator = gameObject.GetComponent<Animator>();
+        heroSpriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+        heroRB = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    public void turnOnOffMovementAnim(bool onOff){
+    	/*
+    	activate and deactivate moviment animation
+    	RECEIVES
+    		onOff: bool
+    	*/
+    	heroAnimator.SetBool("isMoving", !heroAnimator.GetBool("isMoving"));
+    }
+
+    public void move(int destination){
+    	/*
+    	RECEIVES
+    		destination: int
+    		1 is for right
+    		-1 is for left
+    	*/
+    	heroSpriteRenderer.flipX = (destination == -1);
+    	Vector2 direction = new Vector2(destination, 0);
+    	Vector2 currentPosition = (Vector2)transform.position;
+    	transform.position = Vector2.MoveTowards(currentPosition, currentPosition + direction, Time.deltaTime * heroVelocity);
+    }
+
+    public void jump(){
+    	heroAnimator.SetBool("isJumping", true);
+    	heroRB.AddForce(new Vector2(0, jumpForce));
+    }
+
+    public bool triggerJumpAnim(){
+    	if(heroRB.velocity.y != 0){
+			heroAnimator.SetBool("isJumping", true);
+			return true;
+    	}else{
+			heroAnimator.SetBool("isJumping", false);
+			return false;
+    	}
+    }
+}
