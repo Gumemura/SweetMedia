9d4c2eb5339368d3152dab8a6d0010ac9913ebe3
Gumemura <gumemura123@gmail.com>
2021-05-08 18:28:57 -0300

    camera following player implemented

---
+++ Scripts/CameraFollow.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+	public Transform target;
+
+	public float smooth = .2f;
+	public float zOffset = -10;
+
+    // void FixedUpdate()
+    // {
+    // 	float xDest = target.position.x;
+    //     Vector3 destination = new Vector3(xDest, 0, zOffset);
+    //     Vector3 smoothDestination = Vector3.Lerp(transform.position, destination, smooth);
+    //     transform.position = smoothDestination;
+    // }
+
+    void LateUpdate(){
+    	transform.position = new Vector3(target.position.x, 0, zOffset);
+    }
+
+}
